43aea74645866255791213bdd17e128ee6e28784
Long Cui <long.cui@miles.no>
2020-02-17 09:17:05 +0100

    Tested Include method

--- ContosoUniversity/Pages/Students/Details.cshtml.cs
+++ ContosoUniversity/Pages/Students/Details.cshtml.cs
@@ -28,13 +28,16 @@ namespace ContosoUniversity
                 return NotFound();
             }
 
-            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+            Student = await _context.Student
+                //.Include(s => s.Enrollments)
+                .FirstOrDefaultAsync(m => m.ID == id);
+
 
             if (Student == null)
             {
                 return NotFound();
             }
             return Page();
-        }
+        }   
     }
 }
